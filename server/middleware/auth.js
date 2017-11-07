ea06715cb6e71fea6d86799075c3b6a163330ba9
Hack Reactor Student <student@hackreactor.com>
2017-11-07 11:49:33 -0800

    cookie parser complete, sessions added for new users

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,6 +2,19 @@ const models = require('../models');
 const Promise = require('bluebird');
 
 module.exports.createSession = (req, res, next) => {
+  if (!req.cookie) {
+    models.Sessions.create().then(function(session) {
+      var sessionId = session.insertId;
+      models.Sessions.get({ id: sessionId }).then(function(session) {
+        req.session = session;
+        req.session.hash = session.hash;
+        res.cookies['shortlyid'] = {value: session.hash};
+        next();
+      });
+    }).catch(function(err) {
+      console.error(err);
+    });
+  }
 };
 
 /************************************************************/
--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,57 +1,28 @@
 const models = require('../models');
 
-const parseCookies = (req, res, next) => {
-  //console.log('tttttttttttttttttttttttt', req);
-  // console.log('--------------------->', typeof(req.headers));
-  //console.log('--------------->', JSON.stringify(req._setCookiesVariable));
-  // console.log('!!!!!!!!!!!!!!!!!!!!!!!!', res);
-  // console.log(JSON.stringify('?????????????????????????????', req.cookie));
-  var cookie = req.headers.cookie;
-  //console.log('THIS IS THE COOKIE', models.Sessions.create());
-  if (cookie === undefined) {
-    res.cookies = {};
-    // models.Sessions.create().then(function(createdSession) {
-    //   console.log('---------->', createdSession);
-    //   models.Session.get({id: createdSession.insertId}).then(function(session) {
-    //     console.log('8888888888888888888888888888', session);
-    //     res.cookie = session.hash;
-    //   });
-    //   //res.writeHead(statusCode, {createdSession});
-    // });
-    //  create a new session
-    //  store session in session table
-    //  use the hash to assign a new cookie in the response headers
-  } else {
-    console.log(res);
-    var cook = cookie.split(';');
-    console.log('------------>', cook);
-    //cook = cook.join(' ').split('=');
-    console.log('VINOOOOOOOOOOOOOOOOOOOj', cook);
-    for (let i = 0; i < cook.length; i++) {
-      var name = cook[i].indexOf('=');
-      var key = cook[i].substring(0, name);
-      var val = cook[i].substring(name + 1);
-    }
-    if (key[0] === ' ') {
-      key = key.substring(1);
-    }
-    req.cookies[key] = val;
 
-    // cook.forEach((each, index) => {
-    //   if (index % 2 !== 0) {
-    //     req.cookies[cook[index - 1]] = cook[index];
-    //   }
-    // });
-    // var cookieName = cook[0];
-    // req.cookies[cookieName] = cook[1];
-    console.log(req);
-  }
-  //  cookie = JSON.parse(cookie);
-  
-  // if (req.headers.cookies === {}) {
+const parseCookies = (req, res, next) => {
+  req.cookies = {};
+
+  if (req.headers.cookie) {
     
-  // }
-  // console.log(req.headers.cookies);
+    if (req.headers.cookie.length < 60) {
+      req.cookies[req.headers.cookie.substring(0, 9)] = req.headers.cookie.substring(10);
+    } else {
+      var split = req.headers.cookie.split(';');
+      
+      for (var i = 0; i < split.length; i++) {
+        var index = split[i].indexOf('=');
+        var key = split[i].substring(0, index);
+        var val = split[i].substring(index + 1);
+        if (key[0] === ' ') {
+          key = key.substring(1);
+        }
+        req.cookies[key] = val;
+      }
+    }
+  }
+  
   next();
 };
 
